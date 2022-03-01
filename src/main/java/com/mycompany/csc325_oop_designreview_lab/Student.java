5a89a19dcf0a2d0025fb224b06505e978d219f18
StevThePowerful <StevThePowerful@137.125.155.62>
2022-03-01 14:20:53 -0500

    Completed ToDo #1 and #2.

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -9,7 +9,21 @@ package com.mycompany.csc325_oop_designreview_lab;
  *
  * @author MoaathAlrajab
  */
-public class Student {
+public class Student extends Human {
+
+    public Student(String string, short s) {
+        super(string, s);
+    }
+
+    @Override
+    public String getAddress() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void setAddress(String address) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 	// ToDo 1: Make this class a child of Human
 	
 	// ToDo 2: Fix the resulting errors
